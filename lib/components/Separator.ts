7344df93e62e83f25e5f1f62acd892db76f2898d
Colin T.A. Gray <colinta@gmail.com>
2023-07-18 22:22:17 -0400

    update Separator code style

--- lib/components/Separator.ts
+++ lib/components/Separator.ts
@@ -1,11 +1,14 @@
 import type {Viewport} from '../Viewport'
 import type {Props as ViewProps} from '../View'
 import {View} from '../View'
+import type {Style} from '../Style'
 import {Point, Size} from '../geometry'
 
 type Direction = 'vertical' | 'horizontal'
 type Border =
   | 'single'
+  | 'leading'
+  | 'trailing'
   | 'bold'
   | 'dash'
   | 'dash2'
@@ -17,45 +20,53 @@ interface Props extends ViewProps {
   direction: Direction
   padding?: number
   border?: Border
+  style?: Style
 }
 
 export class Separator extends View {
-  readonly direction: Direction
-  readonly padding: number
-  readonly border: Border
+  #direction: Direction
+  #padding: number
+  #border: Border
+  #style: Style | undefined
 
-  constructor({direction, padding, border, ...viewProps}: Props) {
+  constructor({direction, padding, border, style, ...viewProps}: Props) {
     super(viewProps)
-    this.direction = direction
-    this.padding = padding ?? 0
-    this.border = border ?? 'single'
+    this.#direction = direction
+    this.#padding = padding ?? 0
+    this.#border = border ?? 'single'
+    this.#style = style
   }
 
   intrinsicSize(size: Size): Size {
-    if (this.direction === 'vertical') {
-      return new Size(1 + 2 * this.padding, size.height)
+    if (this.#direction === 'vertical') {
+      return new Size(1 + 2 * this.#padding, size.height)
     } else {
-      return new Size(size.width, 1 + 2 * this.padding)
+      return new Size(size.width, 1 + 2 * this.#padding)
     }
   }
 
   render(viewport: Viewport) {
-    if (this.direction === 'vertical') {
-      const [char] = BORDERS[this.border]
-      for (let y = 0; y < viewport.contentSize.height; ++y) {
-        viewport.write(char, new Point(this.padding, y))
+    viewport.usingPen(this.#style, () => {
+      if (this.#direction === 'vertical') {
+        const [char] = BORDERS[this.#border],
+          minY = viewport.visibleRect.minY(),
+          maxY = viewport.visibleRect.maxY()
+        for (let y = minY; y < maxY; ++y) {
+          viewport.write(char, new Point(this.#padding, y))
+        }
+      } else {
+        const [, char] = BORDERS[this.#border]
+        const pt = viewport.visibleRect.origin.offset(0, this.#padding)
+        viewport.write(char.repeat(viewport.visibleRect.size.width), pt)
       }
-    } else {
-      const [, char] = BORDERS[this.border]
-      const pt = viewport.visibleRect.origin.mutableCopy()
-      pt.y += this.padding
-      viewport.write(char.repeat(viewport.visibleRect.size.width), pt)
-    }
+    })
   }
 }
 
 const BORDERS: Record<Border, [string, string]> = {
   single: ['│', '─'],
+  leading: ['▏', '▔'],
+  trailing: ['▕', '▁'],
   bold: ['┃', '━'],
   dash: ['╵', '╴'],
   dash2: ['╎', '╌'],
