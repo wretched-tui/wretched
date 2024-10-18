6a6568f7702aab6c68c5503f5064c77bcbd49adb
Colin T.A. Gray <colinta@gmail.com>
2024-10-17 23:25:47 -0400

    add showPercent prop to Progress

--- lib/components/Progress.ts
+++ lib/components/Progress.ts
@@ -1,20 +1,23 @@
 import {Viewport} from '../Viewport'
 import {type Props as ViewProps, View} from '../View'
 import {Point, Size, interpolate} from '../geometry'
+import {Style} from '../Style'
 
 type Direction = 'vertical' | 'horizontal'
 
 interface Props extends ViewProps {
-  direction: Direction
+  direction?: Direction
   min?: number
   max?: number
   progress?: number
+  showPercent?: boolean
 }
 
 export class Progress extends View {
   #direction: Direction = 'horizontal'
   #range: [number, number] = [0, 100]
   #progress: number = 0
+  #showPercent: boolean = false
 
   constructor(props: Props) {
     super(props)
@@ -26,10 +29,11 @@ export class Progress extends View {
     super.update(props)
   }
 
-  #update({direction, min, max, progress}: Props) {
-    this.#direction = direction
+  #update({direction, min, max, progress, showPercent}: Props) {
+    this.#direction = direction ?? 'horizontal'
     this.#range = [min ?? 0, max ?? 100]
     this.#progress = progress ?? Math.min(...this.#range)
+    this.#showPercent = showPercent ?? false
   }
 
   get progress() {
@@ -45,11 +49,29 @@ export class Progress extends View {
   }
 
   render(viewport: Viewport) {
+    if (viewport.isEmpty) {
+      return
+    }
+
     const pt = Point.zero.mutableCopy()
-    const textStyle = this.theme.ui()
-    const controlStyle = this.theme.ui({isOrnament: true})
+    const percent = this.#showPercent
+      ? `${Math.round(
+          (100 * (this.#progress - this.#range[0])) /
+            (this.#range[1] - this.#range[0]),
+        )}%`
+      : ''
+    const percentStartPoint = new Point(
+      ~~((viewport.contentSize.width - percent.length) / 2),
+      ~~(viewport.contentSize.height / 2),
+    )
+    const textStyle = this.theme.text()
+    const controlStyle = this.theme.ui({isHover: true}).invert()
+    const altTextStyle = new Style({
+      foreground: textStyle.foreground,
+      background: controlStyle.foreground,
+    })
     if (this.#direction === 'horizontal') {
-      const position = Math.round(
+      const progressX = Math.round(
         interpolate(this.#progress, this.#range, [
           0,
           viewport.contentSize.width - 1,
@@ -58,52 +80,66 @@ export class Progress extends View {
       viewport.visibleRect.forEachPoint(pt => {
         let char: string,
           style = textStyle
-        if (pt.x <= position) {
-          if (pt.y === 0 && viewport.contentSize.height > 1) {
-            char = '▄'
-          } else if (
-            pt.y === viewport.contentSize.height - 1 &&
-            viewport.contentSize.height > 1
-          ) {
-            char = '▀'
+        if (
+          this.#showPercent &&
+          pt.x >= percentStartPoint.x &&
+          pt.x - percentStartPoint.x < percent.length &&
+          pt.y === percentStartPoint.y
+        ) {
+          char = percent[pt.x - percentStartPoint.x]
+          if (pt.x <= progressX) {
+            style = altTextStyle
           } else {
-            char = '█'
+            style = textStyle
           }
-          style = controlStyle
-        } else if (viewport.contentSize.height === 1) {
-          if (pt.x === 0) {
-            char = '╶'
-          } else if (pt.x === viewport.contentSize.width - 1) {
-            char = '╴'
-          } else {
-            char = '─'
-          }
-        } else if (pt.y === 0) {
-          if (pt.x === 0) {
-            char = '┌'
-          } else if (pt.x === viewport.contentSize.width - 1) {
-            char = '┐'
-          } else {
-            char = '─'
-          }
-        } else if (pt.y === viewport.contentSize.height - 1) {
-          if (pt.x === 0) {
-            char = '└'
-          } else if (pt.x === viewport.contentSize.width - 1) {
-            char = '┘'
-          } else {
-            char = '─'
-          }
-        } else if (pt.x === 0 || pt.x === viewport.contentSize.width - 1) {
-          char = '│'
         } else {
-          char = ' '
+          if (pt.x <= progressX && this.#progress > this.#range[0]) {
+            if (pt.y === 0 && viewport.contentSize.height > 1) {
+              char = '▄'
+            } else if (
+              pt.y === viewport.contentSize.height - 1 &&
+              viewport.contentSize.height > 1
+            ) {
+              char = '▀'
+            } else {
+              char = '█'
+            }
+            style = controlStyle
+          } else if (viewport.contentSize.height === 1) {
+            if (pt.x === 0) {
+              char = '╶'
+            } else if (pt.x === viewport.contentSize.width - 1) {
+              char = '╴'
+            } else {
+              char = '─'
+            }
+          } else if (pt.y === 0) {
+            if (pt.x === 0) {
+              char = '┌'
+            } else if (pt.x === viewport.contentSize.width - 1) {
+              char = '┐'
+            } else {
+              char = '─'
+            }
+          } else if (pt.y === viewport.contentSize.height - 1) {
+            if (pt.x === 0) {
+              char = '└'
+            } else if (pt.x === viewport.contentSize.width - 1) {
+              char = '┘'
+            } else {
+              char = '─'
+            }
+          } else if (pt.x === 0 || pt.x === viewport.contentSize.width - 1) {
+            char = '│'
+          } else {
+            char = ' '
+          }
         }
 
         viewport.write(char, pt, style)
       })
     } else {
-      const position = Math.round(
+      const progressY = Math.round(
         interpolate(this.#progress, this.#range, [
           viewport.contentSize.height - 1,
           0,
@@ -112,7 +148,7 @@ export class Progress extends View {
       viewport.visibleRect.forEachPoint(pt => {
         let char: string,
           style = textStyle
-        if (pt.y >= position) {
+        if (pt.y >= progressY) {
           if (pt.x === 0 && viewport.contentSize.width > 1) {
             char = '▐'
           } else if (
